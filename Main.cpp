564515017ba16f188e7e6b21d8883cfe2ca0ec7a
MossFrog <kaanozcelik@sabanciuniv.edu>
2015-10-26 13:01:36 +0200

    Implemented the basic physics and skeleton of the game

--- Main.cpp
+++ Main.cpp
@@ -3,22 +3,65 @@
 int main()
 {
 	//--  Set the resolution to 9:16 preferred resolution for mobile phones --//
+	//-- Disable keyrepeat events to make the game tap oriented --//
+	//-- Limit the framerate to prevent screen tearing --//
 	sf::RenderWindow window(sf::VideoMode(405, 720), "Flappy Bird Clone", sf::Style::Close);
+	window.setFramerateLimit(60);
+	window.setKeyRepeatEnabled(false);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	sf::RectangleShape player;
+	player.setSize(sf::Vector2f(100,50));
+	player.setPosition(152,360);
+	player.setFillColor(sf::Color::Red);
 
-        window.clear();
+	int playerFallSpeed = 1;
+	int gravity = 1;
+
+	//-- initialize the main game clock --//
+	sf::Clock mainClock;
+	mainClock.restart();
 
 
-        window.display();
-    }
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
 
-    return 0;
+			//-- If the specified key is pressed push the player upwards --//
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::A)
+				{
+					playerFallSpeed -= 30;
+				}
+			}
+		}
+
+		if(mainClock.getElapsedTime() >= sf::milliseconds(15))
+		{
+			playerFallSpeed += gravity;
+			player.move(0,playerFallSpeed);
+			mainClock.restart();
+		}
+
+
+
+		if(playerFallSpeed <= -30)
+		{
+			playerFallSpeed = -30;
+		}
+
+
+		//-- Any drawing methods after screen clear --//
+		window.clear();
+
+		window.draw(player);
+
+		window.display();
+	}
+
+	return 0;
 }
